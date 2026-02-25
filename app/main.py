a696e4f9e350682ff778e833bef716105a760adc
zeyu80023 <zeyu8023@gmail.com>
2026-02-25 18:35:01 +0800

    新增追剧日历2

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from app.core.config import PORT, SECRET_KEY, CONFIG_DIR, FONT_DIR
 from app.core.database import init_db
 from app.services.bot_service import bot
 # 🔥 引入新路由 webhook
-from app.routers import views, auth, users, stats, bot as bot_router, system, proxy, report, webhook,insight, tasks,history,calendar
+from app.routers import views, auth, users, stats, bot as bot_router, system, proxy, report, webhook,insight,tasks,history,calendar
 
 # 初始化目录和数据库
 if not os.path.exists("static"): os.makedirs("static")
@@ -51,6 +51,7 @@ app.include_router(webhook.router)
 # 🔥 注册 tasks 路由
 app.include_router(tasks.router)
 app.include_router(history.router)
+# 注册 calendar 路由
 app.include_router(calendar.router)
 
 if __name__ == "__main__":
